672ff90746dc2ed1dc5b696a1f1ff564fcf2cb7d
Souravpakhira <pakhira55@gmail.com>
2020-07-24 22:31:50 +0530

    Update ui.js

--- ui.js
+++ ui.js
@@ -41,7 +41,7 @@ class UI {
         <div class="card card-body mb-2">
             <div class="row">
                 <div class="col-md-6">
-                    <a href="${repo.html_url}">${repo.name}
+                    <a href="${repo.html_url}" target="_blank">${repo.name}
                 </div>
                 <div class="col-md-6">
                     <span class="badge badge-pill badge-primary">Stars : ${repo.stargazers_url}</span>
